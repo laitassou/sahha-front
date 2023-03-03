c5a41ba1f19306cc4548595d4d83c781baf95ef4
ZakariaBenali <benalizak97@gmail.com>
2023-03-03 20:50:53 +0100

    Remove unnecessary react import

--- src/components/common/Navbar/NavItem.jsx
+++ src/components/common/Navbar/NavItem.jsx
@@ -1,5 +1,4 @@
 import clsx from 'clsx';
-import React from 'react';
 
 const NavItem = ({ children, className }) => {
 	return <li className={clsx('flex-1 border-b border-dashed lg:border-none lg:flex-none', className)}>{children}</li>;
--- src/components/common/Navbar/NavbarLink.jsx
+++ src/components/common/Navbar/NavbarLink.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const NavBarLink = ({ children, to }) => {
 	return (
 		<a
