cb7acf47c9f44b9b6dc21e33fa2f535235a4f21d
laitassou <lhoussain.aitassou@gmail.com>
2023-03-11 18:58:31 +0100

    fix calender dispaly

--- src/components/SingleAnnonce.tsx
+++ src/components/SingleAnnonce.tsx
@@ -10,13 +10,22 @@ import Selectable from './Calendar';
 require('moment/locale/fr.js');
 const localizer = momentLocalizer(moment);
 
+interface Annonce {
+	id: number;
+	title: string;
+	description: string;
+}
+
 const SingleAnnonce = () => {
-	const auth = localStorage.getItem('authTokens');
+	let [annonce, setAnnonceData] = useState<Annonce>();
+	const auth = localStorage.getItem('authTokens') as string;
+
+
 	const auth_json = JSON.parse(auth);
 	const json_auth_token = auth_json.token;
 	const { user, logoutUser } = useContext(AuthContext);
 
-	const { id } = useParams();
+	const { id } = useParams<{ id: string }>();
 
 	let getSingleAnnonce = async () => {
 		let url = 'http://127.0.0.1:8000/api/slots/' + id + '/';
@@ -30,34 +39,40 @@ const SingleAnnonce = () => {
 		let data = await response.json();
 		setAnnonceData(data);
 	};
-	let [annonce, setAnnonceData] = useState([]);
+
 
 	useEffect(() => {
 		getSingleAnnonce();
 	}, []);
 
-	if (user) {
-		return (
-			<div className="">
-				<div className="container">
-					<div className="connect_box announce_box"></div>
-					<Link to="/list-annonces">Retour aux annonces</Link>
+	if (!user) {
+		return null;
+	}
+	return (
+		<div className="">
+			<div className="container">
+				<div className="connect_box announce_box"></div>
+				<Link to="/list-annonces">Retour aux annonces</Link>
 
-					<div className="container">
-						<div className="announce_box">
-							<h2>Details de votre annonce</h2>
-							<div className="form_box">
-								<h3>{annonce.title} </h3>
-								{annonce.description}
-							</div>
-							<Selectable localizer={localizer} data={annonce} />
-						</div>
-						<div className="navbar"></div>
+				<div className="container">
+					<div className="announce_box">
+						<h2>Details de votre annonce</h2>
+						{annonce && (
+							<>
+								<div className="form_box">
+									<h3>{annonce.title} </h3>
+									{annonce.description}
+								</div>
+								<Selectable localizer={localizer} data={annonce} />
+							</>
+						)}
 					</div>
+					<div className="navbar"></div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
+
 };
 
 export default SingleAnnonce;
