7982e3b79da6d8815d19ce51d09c36b1a9e5ae63
ZakariaBenali <benalizak97@gmail.com>
2023-04-23 18:06:31 +0100

    fix: reset calendar slot on announce change

--- src/components/SingleAnnonce.tsx
+++ src/components/SingleAnnonce.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import AuthContext from '../context/AuthContext';
 import { Link } from 'react-router-dom';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -24,59 +24,53 @@ const SingleAnnonce = () => {
 	let [slots, setSlotsData] = useState<slots[]>([]);
 	const auth = localStorage.getItem('authTokens') as string;
 
-
 	const auth_json = JSON.parse(auth);
 	const json_auth_token = auth_json.token;
-	const { user, logoutUser } = useContext(AuthContext);
+	const { user } = useContext(AuthContext);
 
 	const { id } = useParams<{ id: string }>();
 
-	let getSingleAnnonce = async () => {
-		let url = MAIN_URL + '/slots/' + id + '/';
-		console.log('url:' + url);
-		let response = await fetch(url, {
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: 'Token ' + json_auth_token,
-			},
-		});
-		let data = await response.json();
-		setSlotsData(data);
-	};
-
-
 	useEffect(() => {
+		let getSingleAnnonce = async () => {
+			let url = MAIN_URL + '/slots/' + id + '/';
+			let response = await fetch(url, {
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'Token ' + json_auth_token,
+				},
+			});
+			let data = await response.json();
+			setSlotsData(data);
+		};
 		getSingleAnnonce();
-	}, []);
+	}, [id, json_auth_token]);
 
 	if (!user) {
 		return null;
 	}
 
-
-
 	return (
 		<div className="">
 			<div className="container">
 				<div className="connect_box announce_box"></div>
-				<Link to="/list-annonces"><SectionTitle title="Retour aux annonces" className="text-center" /></Link>
+				<Link to="/list-annonces">
+					<SectionTitle title="Retour aux annonces" className="text-center" />
+				</Link>
 
 				<div className="container">
 					<div className="announce_box">
 						<h2>Details de votre annonce</h2>
-						{(
+						{
 							<>
-
 								<Selectable localizer={localizer} data={slots} anonceid={id} />
 							</>
-						)}
+						}
 					</div>
 					<div className="navbar"></div>
 				</div>
 			</div>
 		</div>
 	);
-
 };
 
 export default SingleAnnonce;
